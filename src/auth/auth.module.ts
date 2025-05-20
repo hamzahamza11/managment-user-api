546f6e780d8c1e88d60bf2d3b5404c3fdbb09e19
hamzahamza11 <sabirha00@gmail.com0>
2025-05-21 00:10:05 +0100

    Enhance authentication module with JWT strategy implementation
    - Updated AuthModule to register JwtStrategy for handling JWT authentication.
    - Configured PassportModule with a default JWT strategy.
    - Implemented JwtStrategy to extract and validate JWT from request headers.
    - Ensured consistency in secret key usage across AuthModule and JwtStrategy.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,22 +3,25 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
-
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { Permission } from '../permissions/entities/permission.entity';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([Permission]),
     JwtModule.register({
       secret: 'votre_secret_jwt_super_securise', // À remplacer par une vraie clé secrète en production
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
   controllers: [AuthController],
   exports: [AuthService],
 })
